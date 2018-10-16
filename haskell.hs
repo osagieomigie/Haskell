6c2de4b85439002ebf2868cf7c5e64204ba9b475
osagieomigie <osagieomigie@gmail.com>
2018-10-15 19:03:02 -0600

    Added a function that finds the smallest factor of an input

--- haskell.hs
+++ haskell.hs
@@ -6,3 +6,11 @@ productFactorial 0 = 1
 productFactorial 1 = 1 
 productFactorial n = n * productFactorial(n-1) * prodFact
                   where prodFact = product [1..n-1]
+
+
+smallestFactor :: Int -> Int 
+smallestFactor n = foldr1 min small 
+                   where small = [x | x <- allDivisors n, x>=2] 
+
+allDivisors :: Int -> [Int]
+allDivisors n = [x | x <- [1..n], n `mod` x == 0]
